87b37f24b255d13408afdffdcbbe403cb2116563
Ruslan <abdullin.dev@gmail.com>
2023-02-04 23:15:12 +0500

    install swagger and described

--- app/Http/Controllers/RSS/RBCController.php
+++ app/Http/Controllers/RSS/RBCController.php
@@ -7,6 +7,15 @@ use App\Services\RSS\RBC\DTO\Storage\ListConditions as ListConditionsDTO;
 use App\Services\RSS\RBC\RBC as RBCService;
 use Illuminate\Http\Request;
 
+/**
+ * @OA\Schema(
+ *     title="RBCController",
+ *     description="RBCController",
+ *     @OA\Xml(
+ *         name="RBCController"
+ *     )
+ * )
+ */
 class RBCController extends Controller
 {
     private RBCService $rbcService;
@@ -16,11 +25,72 @@ class RBCController extends Controller
         $this->rbcService = $rbcService;
     }
 
+    /**
+     * @OA\Get (
+     *   path="/api/rbc/news",
+     *   operationId="/api/rbc/news",
+     *   tags={"Новости"},
+     *   description="Новости из RSS страницы rbc.ru",
+     *
+     *  @OA\Parameter(
+     *      name="page",
+     *      description="Номер страницы",
+     *      in="query",
+     *      required=false,
+     *      @OA\Schema(
+     *           type="integer",
+     *           default=1
+     *      )
+     *   ),
+     *   @OA\Parameter(
+     *      name="sort",
+     *      description="Сортировка по дате публикации новости",
+     *      in="query",
+     *      required=false,
+     *      @OA\Schema(
+     *           type="string"
+     *      )
+     *   ),
+     *   @OA\Parameter(
+     *      name="fields",
+     *      description="Список возвращаемых полей новости",
+     *      in="query",
+     *      required=false,
+     *      @OA\Schema(
+     *          type="string"
+     *      )
+     *   ),
+     *   @OA\Response(
+     *      response=200,
+     *      description="Выведет список новостей из RSS страницы ресурса rbc.ru",
+     *     @OA\JsonContent(
+     *		@OA\Property(
+     *      	description="Статус (success, error)",
+     *          property="status",
+     *          type="string",
+     *     		default="success"
+     *      ),
+     *		@OA\Property(
+     *      	description="Список новостей",
+     *          property="list",
+     *   		type="array",
+     *   		@OA\Items(ref="#/components/schemas/NewsItem")
+     *      ),
+     *		@OA\Property(
+     *      	description="Текст сообщения об ошибке",
+     *          property="message",
+     *          type="string",
+     *          default=""
+     *      )
+     *      )
+     *   )
+     *)
+     */
     public function news(Request $request)
     {
         if ($request->method() != 'GET') {
             return json_encode([
-                'status' => false,
+                'status' => 'error',
                 'message' => 'HTTP-запрос должен содержать метод GET'
             ]);
         }
@@ -43,7 +113,8 @@ class RBCController extends Controller
         $news = $this->rbcService->news($listConditionsDTO);
 
         return json_encode([
-            'status' => true,
+            'status' => 'success',
+            'message' => '',
             'list' => $news
         ]);
     }
--- app/Models/RBCNew.php
+++ app/Models/RBCNew.php
@@ -5,6 +5,54 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @OA\Schema(
+ *     schema="NewsItem",
+ *	   @OA\Property(
+ *      	description="Идентификатор новости",
+ *          property="id",
+ *          type="integer",
+ *     	    nullable=false,
+ *     		example=1
+ *     ),
+ *     @OA\Property(
+ *      	description="Заголовок новости",
+ *          property="name",
+ *          type="string",
+ *     	    nullable=false,
+ *     		example="Новость 1"
+ *     ),
+ *     @OA\Property(
+ *      	description="Краткое описание новости",
+ *          property="short_description",
+ *          type="string",
+ *     	    nullable=false,
+ *     		example="Краткое описание"
+ *     ),
+ *     @OA\Property(
+ *      	description="Дата публикации новости",
+ *          property="public_date",
+ *          type="string",
+ *     	    nullable=false,
+ *     		example="2023-02-04 08:45:51"
+ *     ),
+ *     @OA\Property(
+ *      	description="Автор новости",
+ *          property="author",
+ *          type="string",
+ *     	    nullable=true,
+ *     		example="Иван Иваныч"
+ *     ),
+ *     @OA\Property(
+ *      	description="Картинка новости",
+ *          property="image",
+ *          type="string",
+ *     	    nullable=true,
+ *     		example="/storage/images/rbc/756460582323479.jpg"
+ *     )
+ * )
+ *
+ */
 class RBCNew extends Model
 {
     use HasFactory;
